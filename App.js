673f77f6fb6fe745325d0c07a08a5f497ba1fb44
Nicolas Geoffroy <81689515+ngeoffroy@users.noreply.github.com>
2022-03-17 09:29:03 -0300

    navigator container

--- App.js
+++ App.js
@@ -5,15 +5,11 @@ import {
   GestureHandlerRootView,
   RectButton,
 } from 'react-native-gesture-handler'
-
 const App = () => {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
-    <NavigationContainer>
       <TabNavigator />
-    </NavigationContainer>
     </GestureHandlerRootView>
   );
 };
-
 export default App;
