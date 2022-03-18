68197c727628bf2d72fc1f5f2a6096ab17bcff8b
Nicolas Geoffroy <81689515+ngeoffroy@users.noreply.github.com>
2022-03-18 08:05:49 -0300

    tab navigator fix

--- TabNavigator.js
+++ TabNavigator.js
@@ -1,25 +1,41 @@
 import React from "react";
+
 import "react-native-gesture-handler";
+
 import { Image, View } from "react-native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import Home from "./pantallas/Principal";
+
 import Qr from "./pantallas/Qr";
+
 import Perfil from "./pantallas/Perfil";
-import MisCursos from "./pantallas/MisCursos";
-import Favoritos from "./pantallas/Favoritos";
+
 import AvanceCursos from "./pantallas/AvanceCursos";
+
 import EditarPerfil from "./pantallas/EditarPerfil";
+
 import Login from "./pantallas/Login";
+
 import Categorias from "./pantallas/Categorias";
+
 import Registro from "./pantallas/Registro";
+
 import Marcas from "./pantallas/Marcas";
+
 import Splash from "./pantallas/Splash";
+
 import Recuperar from "./pantallas/Recuperar";
+
 import PantallaSlides from "./componentes/Slider";
+
 import { FontAwesome } from "react-native-vector-icons";
+
 import { createDrawerNavigator } from "@react-navigation/drawer";
+
 import { NavigationContainer } from '@react-navigation/native';
+
 const Tab = createDrawerNavigator();
 function LogoTitle() {
   return (
@@ -39,10 +55,9 @@ function HomeStackScreen() {
     >
       <HomeStack.Screen name="Login" component={Login} />
       <HomeStack.Screen name="Mi Perfil" component={Perfil} />
-      <HomeStack.Screen name="Mis Cursos" component={MisCursos} />
-      <HomeStack.Screen name="Mis Favoritos" component={MisCursos} />
+      <HomeStack.Screen name="Mis Cursos" component={Perfil} />
+      <HomeStack.Screen name="Mis Favoritos" component={Perfil} />
       <HomeStack.Screen name="AvanceCursos" component={AvanceCursos} />
-      <HomeStack.Screen name="Favoritos" component={Favoritos} />
       <HomeStack.Screen name="EditarPerfil" component={EditarPerfil} />
       <HomeStack.Screen name="PantallaSlides" component={PantallaSlides} />
       <HomeStack.Screen name="Registro" component={Registro} />
@@ -59,7 +74,7 @@ function InicioStackScreen() {
       }}
     >
       <InicioStack.Screen name="Mi Perfil" component={Home} />
-      <InicioStack.Screen name="Mis Cursos" component={MisCursos} />
+      <InicioStack.Screen name="Mis Cursos" component={Perfil} />
       <InicioStack.Screen name="Categorias" component={Categorias} />
       <InicioStack.Screen name="Marcas" component={Marcas} />
     </InicioStack.Navigator>
